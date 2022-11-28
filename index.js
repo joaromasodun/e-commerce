94b5ae1501f9b2a10ed8f0c82a536507dce701ad
Olayinka Aromasodun <OAromasodun@chiplc.com>
2022-11-28 15:41:51 +0100

    finshed the designs with more features like login to be added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const products = document.querySelectorAll('.product')
-const ctaLists = document.querySelector('.cta-lists')
+const ctaLists = document.querySelectorAll('.cta-lists')
 
+function ShowClassList(i) {
+          ctaLists[i].classList.add('show-cta')
+}
 
 products.forEach(product => {
           product.addEventListener('mouseover', function() {
-                    alert('Hello')
+                    ctaLists.forEach((ctaList, i) => {
+                              ShowClassList(i)
+                    })
           })
+
 })
+
+
+// ctaLists.classList.add('show-cta')
+
+//   ctaLists.classList.remove('show-cta')
