c46dee6f9f787a3db2a17c9d6e5573c1d5ca0539
joaromasodun <joaromasodun@gmail.com>
2022-12-01 10:03:32 +0100

    latest update

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const products = document.querySelectorAll('.product')
-const ctaLists = document.querySelectorAll('.cta-lists')
+const hamburger = document.querySelector('.hamburger')
+const navLists = document.querySelector('.nav-lists')
 
-function ShowClassList(i) {
-          ctaLists[i].classList.add('show-cta')
-}
-
-products.forEach(product => {
-          product.addEventListener('mouseover', function() {
-                    ctaLists.forEach((ctaList, i) => {
-                              ShowClassList(i)
-                    })
-          })
-
+hamburger.addEventListener('click', function() {
+	navLists.classList.add('swipe')
+	console.log(navLists)
 })
-
-
-// ctaLists.classList.add('show-cta')
-
-//   ctaLists.classList.remove('show-cta')
