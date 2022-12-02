7bf9282be743eb66edb521f6351f7895d5e07553
joaromasodun <joaromasodun@gmail.com>
2022-12-02 15:11:56 +0100

    media query added

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const hamburger = document.querySelector('.hamburger')
 const navLists = document.querySelector('.nav-lists')
 
 hamburger.addEventListener('click', function() {
-	navLists.classList.add('swipe')
-	console.log(navLists)
+	navLists.classList.toggle('swipe')
 })
